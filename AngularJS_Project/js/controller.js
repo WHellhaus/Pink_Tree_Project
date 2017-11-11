4ec1e7295ed136cb0871c0a8fcf74a61e602e3e0
WHellhaus <wolfgang.hellicksons@gmail.com>
2017-11-10 18:01:57 -0800

    Like/Dislike Toggles

--- AngularJS_Project/js/controller.js
+++ AngularJS_Project/js/controller.js
@@ -1,9 +1,79 @@
 var insertModule = angular.module('insertModule', []);
+var isLiked = false;
+var isDisliked = false;
 
 insertModule.controller('ListController', ['$scope', '$http', function($scope, $http){
-	//use $http.get() to get the list of students
+
+	$scope.plusOne = function(index) {
+			var data = {
+				id : $scope.productList[index].product_id
+			}
+
+			var like1 = parseInt($scope.productList[index].likes) + 1;
+			$scope.productList[index].likes = like1.toString();
+
+			$http.post('php/updateLikes.php', JSON.stringify(data));
+ }
+
+
+ $scope.minusOne = function(index) {
+	 var data = {
+		 id : $scope.productList[index].product_id
+	 }
+
+	 var dislike1 = parseInt($scope.productList[index].dislikes) + 1;
+	 $scope.productList[index].dislikes = dislike1.toString();
+
+	 $http.post('php/updateDislikes.php', JSON.stringify(data));
+ }
+
+ $scope.undoPlus = function(index) {
+	 var data = {
+		 id : $scope.productList[index].product_id
+	 	 }
+
+	 var like1 = parseInt($scope.productList[index].likes) - 1;
+	 $scope.productList[index].likes = like1.toString();
+
+	 $http.post('php/undoLikes.php', JSON.stringify(data));
+ }
+
+ $scope.undoMinus = function(index) {
+	var data = {
+		id : $scope.productList[index].product_id
+	}
+
+	var dislike1 = parseInt($scope.productList[index].dislikes) - 1;
+	$scope.productList[index].dislikes = dislike1.toString();
+
+	$http.post('php/undoDislikes.php', JSON.stringify(data));
+ }
+
+ $scope.toggleLike = function(index) {
+	 if (isLiked === false) {
+		 isLiked = true;
+		 $scope.plusOne(index);
+	 }
+	 else {
+		 isLiked = false;
+		 $scope.undoPlus(index);
+	 }
+ }
+
+ $scope.toggleDislike = function(index) {
+	if (isDisliked === false) {
+		isDisliked = true;
+		$scope.minusOne(index);
+	}
+	else {
+		isDisliked = false;
+		$scope.undoMinus(index);
+	}
+ }
+
+	//use $http.get() to get the list of items
 	$http.get('php/list.php').then(function(response){
-		//send back the student data to the list.html view
+		//send back the item data to the list.html view
 		$scope.productList = response.data;
 	});
 }]);
---
+++ AngularJS_Project/php/updateDislikes.php
@@ -0,0 +1,9 @@
+<?php
+require('config.php');
+
+$data = json_decode(file_get_contents("php://input"));
+
+$id = mysqli_real_escape_string($con, $data->id);
+
+$query = mysqli_query($con, "update tbl_products set dislikes=dislikes+1 where product_id='$id'") or die ('Unable to execute query. '. mysqli_error($con));
+?>
